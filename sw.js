0b1aadbb86f493e990d051fa90eb64803be639d7
Wout Depotter <61108827+Wout-Depotter@users.noreply.github.com>
2026-04-23 04:20:31 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Savings App - Service Worker v5.5
-const CACHE_VERSION = ‘savings-v5.43’;
+const CACHE_VERSION = ‘savings-v5.45’;
 const CACHE_ASSETS = [’./index.html’];
 
 self.addEventListener(‘install’, event => {
